adc7052306f051370934eeab975419f2433c0083
david-loh01 <davidloh810@gmail.com>
2026-02-20 19:01:31 -0500

    Updated Directory Names
    - Clearer directory and file names
    - Deleted old unclear directory and file names

--- src/models_eval.py
+++ src/models_eval.py
@@ -7,7 +7,6 @@ import numpy as np
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-#from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
@@ -82,16 +81,16 @@ def eval_logreg(X: np.ndarray, y: np.ndarray, title_suffix: str):
         val_y,
         pred_y,
         f"Confusion Matrix (LogReg, {title_suffix})",
-        subdir="confusion_logreg",
-        filename=f"logreg_{title_suffix.replace(':', '_')}.png",
+        subdir="confusion_matrix_logreg",
+        filename=f"confusion_matrix_logreg_{title_suffix.replace(':', '_')}.png",
     )
 
     _plot_precision_recall_curve(
         val_y,
         y_scores,
         f"Precision Recall Curve (LogReg, {title_suffix})",
-        subdir="pr_logreg",
-        filename=f"pr_logreg_{title_suffix.replace(':', '_')}.png",
+        subdir="precision_recall_logreg",
+        filename=f"precision_recall_logreg_{title_suffix.replace(':', '_')}.png",
     )
 
 
@@ -108,15 +107,15 @@ def eval_decision_tree(X: np.ndarray, y: np.ndarray, title_suffix: str):
         val_y,
         y_pred,
         f"Confusion Matrix (DT, {title_suffix})",
-        subdir="confusion_dt",
-        filename=f"dt_{title_suffix.replace(':', '_')}.png",
+        subdir="confusion_matrix_decision_tree",
+        filename=f"confusion_matrix_decision_tree_{title_suffix.replace(':', '_')}.png",
     )
     _plot_precision_recall_curve(
         val_y,
         y_scores,
         f"PR Curve (DT, {title_suffix})",
-        subdir="pr_dt",
-        filename=f"pr_dt_{title_suffix.replace(':', '_')}.png",
+        subdir="precision_recall_decision_tree",
+        filename=f"precision_recall_decision_tree_{title_suffix.replace(':', '_')}.png",
     )
 
 def eval_xgboost(X: np.ndarray, y: np.ndarray, title_suffix: str):
@@ -141,15 +140,15 @@ def eval_xgboost(X: np.ndarray, y: np.ndarray, title_suffix: str):
         val_y,
         y_pred,
         f"Confusion Matrix (XGB, {title_suffix})",
-        subdir="confusion_xgb",
-        filename=f"xgb_{title_suffix.replace(':', '_')}.png",
+        subdir="confusion_matrix_xgboost",
+        filename=f"confusion_matrix_xgboost_{title_suffix.replace(':', '_')}.png",
     )
     _plot_precision_recall_curve(
         val_y,
         y_scores,
         f"PR Curve (XGB, {title_suffix})",
-        subdir="pr_xgb",
-        filename=f"pr_xgb_{title_suffix.replace(':', '_')}.png",
+        subdir="precision_recall_xgboost",
+        filename=f"precision_recall_xgboost_{title_suffix.replace(':', '_')}.png",
     )
 
 def eval_random_forest(X: np.ndarray, y: np.ndarray, title_suffix: str):
@@ -171,13 +170,13 @@ def eval_random_forest(X: np.ndarray, y: np.ndarray, title_suffix: str):
         val_y,
         y_pred,
         f"Confusion Matrix (RF, {title_suffix})",
-        subdir="confusion_rf",
-        filename=f"rf_{title_suffix.replace(':', '_')}.png",
+        subdir="confusion_matrix_random_forest",
+        filename=f"confusion_matrix_random_forest_{title_suffix.replace(':', '_')}.png",
     )
     _plot_precision_recall_curve(
         val_y,
         y_scores,
         f"PR Curve (RF, {title_suffix})",
-        subdir="pr_rf",
-        filename=f"pr_rf_{title_suffix.replace(':', '_')}.png",
+        subdir="precision_recall_random_forest",
+        filename=f"precision_recall_random_forest_{title_suffix.replace(':', '_')}.png",
     )
